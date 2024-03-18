2bc2e6d3fbb8cd51a85311f363ede4930fda22c8
minhnguyen <minhcola9999@gmail.com>
2024-03-18 15:56:17 +0700

    using Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import dogimg from '../../public/dog.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <div className="font-thin">Hello</div>
+      <Image
+      src={'https://picsum.photos/seed/picsum/200/300'}
+      alt="Picture"
+      width={200}
+      height={200}
+      quality={100}
+    />
     </main>
   );
 }
